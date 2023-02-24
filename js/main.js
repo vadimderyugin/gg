fa44e07b1417fcf28e04133744d92277e975cf34
vadimderyugin <vadimderyugin10reg@gmail.com>
2023-02-24 20:00:40 +0600

    feat : finish game

--- js/main.js
+++ js/main.js
@@ -2,20 +2,28 @@ let input = document.querySelector('.input')
 const text = document.querySelector('.text')
 const btn = document.querySelector('.button')
 const start = document.querySelector('.start')
-let conceivedNumber = Math.floor(Math.random() * 100)
+const body = document.querySelector('.body')
+let conceivedNumber
 let numValue
 let counter = 0
 
+const randomNum = () => {
+	return conceivedNumber = Math.floor(Math.random() * 100)
+}
+
 start.addEventListener('click', () => {
+	input.value = ''
+	randomNum()
 	btn.classList.add('active')
 	input.classList.add('active')
-	console.log(conceivedNumber);
+	body.style.backgroundColor = '#edc966'
 	text.textContent = 'Начинаем! Введи число от 0 до 100'
 })
 
 btn.addEventListener('click', () => {
 	numValue = input.value
 	if (conceivedNumber == numValue) {
+		body.style.backgroundColor = '#8bff6b'
 		text.textContent = `Верно! Количество попыток ${counter += 1}`
 	} else if (numValue > 100 || numValue < 0) {
 		text.textContent = 'Введи число от 0 до 100!'
